67658b155e728b549c82c0066b0306f59fdd2dad
nemoyworks <nemoyworks@gmail.com>
2026-06-22 16:47:35 +0300

    Add drag gesture animation snippet

---
+++ Animations/Animations/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Animations
+//
+//  Created by Ростислав Гайда on 08.06.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
